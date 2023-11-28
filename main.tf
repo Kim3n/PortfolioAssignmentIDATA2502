e9029f3c1ba955565ca2f7148e4ce0a15816f343
Kim <lademokim@hotmail.com>
2023-11-28 19:18:14 +0100

    Update main and terraform

--- main.tf
+++ main.tf
@@ -13,10 +13,9 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.google_credentials)
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
@@ -24,11 +23,11 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "example_instance" {
-  name         = "example-instance"
-  machine_type = "n1-standard-1"
-  zone         = var.zone
-  tags         = ["web"]
+  name                    = "example-instance"
+  machine_type            = "n1-standard-1"
+  tags                    = ["web"]
   metadata_startup_script = file("./install-scripts/install.sh")
+
   boot_disk {
     initialize_params {
       image = "ubuntu-2004-focal-v20231101"
@@ -37,13 +36,11 @@ resource "google_compute_instance" "example_instance" {
 
   network_interface {
     network = google_compute_network.vpc_network.name
-
     access_config {
       // Allocate a public IP address to the instance, GCP allocates public IP address by default with empty block
     }
   }
 }
-
 resource "google_compute_firewall" "allow-http" {
   name    = "allow-http"
   network = google_compute_network.vpc_network.name
