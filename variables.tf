b2569643a6d911852187c7a8d3788dc7557630cb
Kim <lademokim@hotmail.com>
2023-11-28 19:19:16 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,10 +12,6 @@ variable "zone" {
   default     = "us-east1-b"
 }
 
-variable "google_credentials" {
-  description = "Path to the Google Cloud credentials file"
-}
-
 variable "html_content" {
   description = "HTML content to display on the webserver"
   default     = "<h1>Hello, Terraform!</h1>"
